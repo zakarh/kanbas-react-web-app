b3fbc536cf62acaec7d87659afe66e01d787c193
user <handricken@NORTHEASTERN.EDU>
2024-09-20 20:27:00 -0400

    Add .

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
 
 export default function TOC() {
-    return (
+  return (
+    <ul>
+      <li className="nav-item">
+        <Link to="/Labs">Labs</Link></li>
+      <li className="nav-item">
+        <Link to="/Labs/Lab1">Lab 1</Link></li>
+      <li className="nav-item">
+        <Link to="/Labs/Lab2">Lab 2</Link></li>
       <ul>
-        <li><Link to="/Labs">Labs</Link></li>
-        <li><Link to="/Labs/Lab1">Lab 1</Link></li>
-        <li><Link to="/Labs/Lab2">Lab 2</Link></li>
-        <li><Link to="/Labs/Lab3">Lab 3</Link></li>
-        <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li><Link to="/Labs/Flex">Lab 2 - Flex</Link></li>
+        <li><Link to="/Labs/ReactIcons">Lab 2 - ReactIcons</Link></li>
+        <li><Link to="/Labs/BootstrapGrids">Lab 2 - BootstrapGrids</Link></li>
+        <li><Link to="/Labs/ScreenSizeLabel">Lab 2 - ScreenSizeLabel</Link></li>
+        <li><Link to="/Labs/BootstrapTables">Lab 2 - BootstrapTables</Link></li>
+        <li><Link to="/Labs/BootstrapLists">Lab 2 - BootstrapLists</Link></li>
+        <li><Link to="/Labs/BootstrapForms">Lab 2 - BootstrapForms</Link></li>
+        <li><Link to="/Labs/BootstrapNavigation">Lab 2 - BootstrapNavigation</Link></li>
       </ul>
-    );
-  }
-  
+      <li><Link to="/Labs/Lab3">Lab 3</Link></li>
+      <li><Link to="/Kanbas">Kanbas</Link></li>
+    </ul>
+  );
+}
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,7 +1,15 @@
-import Lab1 from "./Lab1";
 import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
+import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
+import Flex from "./Lab2/Flex";
+import ReactIcons from "./Lab2/ReactIcons";
+import BootstrapGrids from "./Lab2/BootstrapGrids";
+import ScreenSizeLabel from "./Lab2/ScreenSizeLabel";
+import BootstrapTables from "./Lab2/BootstrapTables";
+import BootstrapLists from "./Lab2/BootstrapLists";
+import BootstrapForms from "./Lab2/BootstrapForms";
+import BootstrapNavigation from "./Lab2/BootstrapNavigation";
 import Lab3 from "./Lab3";
 export default function Labs() {
   return (
@@ -12,6 +20,14 @@ export default function Labs() {
         <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
+        <Route path="Flex" element={<Flex />} />
+        <Route path="ReactIcons" element={<ReactIcons />} />
+        <Route path="BootstrapGrids" element={<BootstrapGrids />} />
+        <Route path="ScreenSizeLabel" element={<ScreenSizeLabel />} />
+        <Route path="BootstrapTables" element={<BootstrapTables />} />
+        <Route path="BootstrapLists" element={<BootstrapLists />} />
+        <Route path="BootstrapForms" element={<BootstrapForms />} />
+        <Route path="BootstrapNavigation" element={<BootstrapNavigation />} />
         <Route path="Lab3" element={<Lab3 />} />
       </Routes>
     </div>
